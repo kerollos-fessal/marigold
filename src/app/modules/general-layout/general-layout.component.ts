3231a18bf7243f7508383c0be4cf52b546436f87
Kerollos Fessal <kerofessal77@gmail.com>
2025-05-24 00:32:38 +0300

    navbar component

--- src/app/modules/general-layout/general-layout.component.ts
+++ src/app/modules/general-layout/general-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-general-layout',
   templateUrl: './general-layout.component.html',
   styleUrls: ['./general-layout.component.scss'],
 
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
 })
 export class GeneralLayoutComponent {}
