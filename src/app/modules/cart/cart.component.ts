ba62bff0bb4e95e39ea59a7cfb8e167f50abfd66
Kerollos Fessal <kerofessal77@gmail.com>
2025-05-23 18:00:46 +0300

    install libraries

--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
-  standalone: true,
   imports: [],
 })
 export class CartComponent {}
