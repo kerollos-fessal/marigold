f6a8a231d1a9dd02dab6acdbdbf6d4c585fb5f5e
Kerollos Fessal <kerofessal77@gmail.com>
2025-05-26 00:25:22 +0300

    all products page

--- src/app/modules/shop/shop-routing.module.ts
+++ src/app/modules/shop/shop-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+     { path: '', component: ShopComponent },
+     { path: ':prouctId/details', component: ProductDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
