dba7e45f3c9ce2314404bfea0e195e5de791e8bc
Kerollos Fessal <kerofessal77@gmail.com>
2025-06-03 22:53:09 +0300

    manage password and my orders screens

--- src/app/modules/profile/components/password-management/password-management.component.ts
+++ src/app/modules/profile/components/password-management/password-management.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { newPasswordMatchValidator } from 'src/app/shared/validators/match-new-passwords';
 
 @Component({
   selector: 'app-password-management',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './password-management.component.html',
   styleUrl: './password-management.component.scss'
 })
-export class PasswordManagementComponent {
+export class PasswordManagementComponent implements OnInit{
+ updatePasswordForm!: FormGroup;
+  hidePassword = true;
+  hideNewPassword = true;
+  hideConfirmPassword = true;
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.initUpdateProfileForm();
+  }
+
+initUpdateProfileForm(){
+    this.updatePasswordForm = this.fb.group({
+      password: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    }, { validators: newPasswordMatchValidator });
+}
+
+onSubmit(): void {
+    if (this.updatePasswordForm.valid) {
+        console.log('Form submitted:', this.updatePasswordForm.value);
+    }
+}
 }
---
+++ src/app/shared/models/orders/order-info.interface.ts
@@ -0,0 +1,6 @@
+export interface OrderInfo {
+  orderId: string;
+  paymentMethod: string;
+  transactionId: string;
+  deliveryDate: string;
+}
